890672c15b47b44d589445566473a196f713e6ab
Deniz Hürmeydan <denizhurmeydan@gmail.com>
2023-07-29 15:50:18 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-task-form").addEventListener("click", (e)=> {
+    e.preventDefault()
+  })
 });
